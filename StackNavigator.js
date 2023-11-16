fc3530d72920fbcdedea6080304558e1ef67c174
Varune <varunetomar@gmail.com>
2023-11-16 04:35:48 -0500

    headerremove

--- StackNavigator.js
+++ StackNavigator.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
 import { AuthOpen } from './hooks/useAuth';
@@ -7,20 +7,23 @@ import { AuthOpen } from './hooks/useAuth';
 const Stack = createNativeStackNavigator();
 
 const StackNavigator = () => {
-  const { user , setUser } = AuthOpen()
-  return (
-    <Stack.Navigator>
-        {user ? (
-            <>
-      <Stack.Screen name="Home" component={HomeScreen} />
-      </>
-        ) : (
-      <Stack.Screen name="Login" component={LoginScreen} />
-        
-        )}
+  const { user, setUser } = AuthOpen();
 
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false, // Set headerShown to false to hide the header for the entire stack
+      }}
+    >
+      {user ? (
+        <>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </>
+      ) : (
+        <Stack.Screen name="Login" component={LoginScreen} />
+      )}
     </Stack.Navigator>
   );
 };
 
-export default StackNavigator
+export default StackNavigator;
